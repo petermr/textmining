96c621a46e261254cca4309df3be2bac46aae18d
petermr <peter.murray.rust@googlemail.com>
2022-07-29 11:20:54 +0100

    tidied output a bit

--- patent/nltk.py
+++ patent/nltk.py
@@ -1,22 +1,38 @@
+import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 from collections import Counter
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+import string
 
+"""Tutorial https://realpython.com/nltk-nlp-python/ """
 class AmiTokenizer:
-    def __init__(self):
+    def __init__(self, use_stopwords=True, remove_punct=True):
         self.text = None
-        self.sentences = None
-        self.words = None
+        self.sentences = []
+        self.words = []
         self.counter = Counter()
+        self.stop_words = set() if not use_stopwords else set(stopwords.words("english"))
+        self.remove_punct = remove_punct
+
 
     def read_text(self, path):
-        with open(path, "r") as f:
-            self.text = f.read()
-        self.tokenize_to_sentences_and_words()
+        if path.exists():
+            with open(path, "r") as f:
+                self.text = f.read()
+            self.tokenize_to_sentences_and_words()
 
+
+    SYMBOLS = ['±', '“']
     def tokenize_to_sentences_and_words(self):
         if self.text:
             self.sentences = sent_tokenize(self.text)
             self.words = word_tokenize(self.text)
+            self.words = [word for word in self.words if word.casefold() not in self.stop_words]
+            if self.remove_punct:
+                self.words = [word for word in self.words if not word in string.punctuation]
+            self.words = [word for word in self.words if not word.isnumeric()]
+            self.words = [word for word in self.words if word not in self.SYMBOLS]
             for word in self.words:
                 self.counter[word] += 1
 
--- test/test_nltk.py
+++ test/test_nltk.py
@@ -2,15 +2,31 @@ from pathlib import Path
 
 from test_lens import RESOURCE_DIR
 from patent.nltk import AmiTokenizer
+from collections import Counter
 
 class TestNltk:
 
     def test_words_sentences_1(self):
         ami_tokenizer = AmiTokenizer()
         ami_tokenizer.read_text(Path(RESOURCE_DIR, "desc_1.txt"))
-        print(f"sentences {len(ami_tokenizer.sentences)}")
-        print(f"words {len(ami_tokenizer.words)}")
+        with open(Path(RESOURCE_DIR, "desc_1.txt")) as f:
+            text = f.read()
+        print(f"text: {text[:500]}...")
+        print(f"sentences {len(ami_tokenizer.sentences)}, words {len(ami_tokenizer.words)}")
         print(f"counter {ami_tokenizer.counter}")
-        ami_tokenizer.counter.most_common(10)
-        # print(f"most common {ww}")
+        print(f"common {ami_tokenizer.counter.most_common(10)}")
+
+    def test_words_sentences_100(self):
+        total_counter = Counter()
+        for i in range(1,100):
+            ami_tokenizer = AmiTokenizer()
+
+            ami_tokenizer.read_text(Path(RESOURCE_DIR, f"desc_{i}.txt"))
+            if ami_tokenizer.sentences:
+                print(f"sentences {len(ami_tokenizer.sentences)}, words {len(ami_tokenizer.words)}")
+                # print(f"counter {ami_tokenizer.counter}")
+                print(f"commonest {ami_tokenizer.counter.most_common(50)}")
+                total_counter += ami_tokenizer.counter
+        print(f"total counter {total_counter}")
+
 
